128ec42a4fdaa7437002b450ed1c1a3a9d7dc4fe
EricLam404 <lameric9888@gmail.com>
2022-09-20 20:56:08 -0400

    Added menu

--- src/menu.js
+++ src/menu.js
@@ -3,7 +3,7 @@ import createNavbar from "./components";
 function loadMenu(){
     removeChildren();
     createNavbar();
-
+    menu()
 }
 
 
@@ -13,5 +13,36 @@ function removeChildren(){
     while(content.firstChild){
         content.removeChild(content.lastChild);
     }
+}
+
+function menu(){
+    let menu = [["Cheese Pizza", 10],
+        ["Pepperoni Pizza", 12],
+        ["Supreme Pizza", 12],
+        ["White Pizza", 10],
+        ["Buffalo Chicken Pizza", 12]
+    ];
+
+    let content = document.getElementById('content');
+
+    for(let i = 0; i < menu.length; i++){
+        content.append(createCard(menu[i][0], menu[i][1]));
+    }
+
+}
+
+function createCard(item, cost){
+    let card = document.createElement('div');
+    card.classList.add("card");
+
+    let name = document.createElement('div');
+    let price = document.createElement('div');
+
+    name.textContent = item;
+    price.textContent = "$" + cost;
+
+    card.append(name, price);
+
+    return card;
 }
 export default loadMenu;
